1aa0795c9017f25ac31b65a90feeccf116711b1f
Caleb <caleb.fangmeier@gmail.com>
2016-01-13 17:10:21 -0600

    Adds simple script to run on all input files

---
+++ SelectionAndReweighting.sh
@@ -0,0 +1,4 @@
+./ZTT_XSection.exe DYJetsToLL.root  root://cmseos.fnal.gov://store/user/abdollah/ROOTHadd/DYJetsToLL.root  
+./ZTT_XSection.exe TTJets.root      root://cmseos.fnal.gov://store/user/abdollah/ROOTHadd/TTJets.root 
+./ZTT_XSection.exe WJetsToLNu.root  root://cmseos.fnal.gov://store/user/abdollah/ROOTHadd/WJetsToLNu.root 
+./ZTT_XSection.exe SingleMu.root    root://cmseos.fnal.gov://store/user/abdollah/ROOTHadd/SingleMu.root  
